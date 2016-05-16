bb2f6fd653a25ad3c4415212677359eaa3aea095
Dennis Hongsi Fang <dfang12@gmail.com>
2016-05-16 00:57:13 -0700

    added form to submit_document for text submission

--- chronos/submit_document/views.py
+++ chronos/submit_document/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
+# Create your views here.
+@csrf_exempt
 def index(request):
-	template = loader.get_template('submit_document/index.html')
-	return HttpResponse(template.render(request))
+
+	if request.method == 'GET':
+		template = loader.get_template('submit_document/index.html')
+		return HttpResponse(template.render(request))
+	if request.method == 'POST':
+		template = loader.get_template('submit_document/index.html')
+		return HttpResponse(template.render(request))
