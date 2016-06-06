6449397ac5e531f3b014955682b2fed4d5def759
Eric Birdsall <eric.a.birdsall@gmail.com>
2016-06-06 02:15:10 -0700

    change contact to about us page

--- chronos/submit_document/urls.py
+++ chronos/submit_document/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 	url(r'^submit_success/(?P<address>[\w\-]+)/$', views.submit_success, name='submit_success'),
 	url(r'^verify_document/$', views.verify_document, name='verify_document'),
 	url(r'^document_query/$',views.document_query, name='document_query'),
+	url(r'^about/$',views.about, name='about'),
 ]
